8cf370ad6c1ab674d91b6f1668615c7176c3a52b
renildomarcio <krsolucoesweb@gmail.com>
2019-06-02 06:53:49 -0300

    Update main.lua

--- esx_blowtorch/client/main.lua
+++ esx_blowtorch/client/main.lua
@@ -57,20 +57,6 @@ AddEventHandler('esx_blowtorch:stopblowtorching', function()
 	ESX.ShowNotification('cancelo blowtorch')
 end)
 
---RegisterNetEvent('esx_blowtorch:opendoors')
---AddEventHandler('esx_blowtorch:opendoors', function(x,y,z)
-	--Citizen.CreateThread(function()
-	--while dooropen do
-		--Wait(5000)
-		--ESX.ShowNotification('abrete sesamo')
-		--local obs, distance = ESX.Game.GetClosestObject('V_ILEV_GB_VAULDR', {x,y,z})
-		--local pos = GetEntityCoords(obs);
-		--ESX.ShowNotification(' hola' .. distance)
-		--SetEntityHeading(obs, GetEntityHeading(obs) + 70.0)
-	--end
-	--end)
---end)
-
 function blowtorchAnimation()
 	local playerPed = GetPlayerPed(-1)
 	blowtorchingtime = 300
@@ -81,8 +67,6 @@ function blowtorchAnimation()
 			Citizen.CreateThread(function()
 				while blowtorching do
 						Wait(2000)
-						--local weld = ESX.Game.GetClosestObject('prop_weld_torch', GetEntityCoords(GetPlayerPed(-1)))
-						--ESX.Game.DeleteObject(weld)
 						TaskStartScenarioInPlace(playerPed, "WORLD_HUMAN_WELDING", 0, true)
 						ESX.ShowNotification('pongo anim')
 						blowtorchingtime = blowtorchingtime - 1
@@ -93,38 +77,8 @@ function blowtorchAnimation()
 						end
 				end
 			end)
-			
-			--while blowtorching do
-				--TaskPlayAnim(playerPed, "amb@world_human_const_blowtorch@male@blowtorch@base", "base", 2.0, 1.0, 5000, 5000, 1, true, true, true)
+
 				TaskPlayAnim(playerPed, "atimetable@reunited@ig_7", "thanksdad_bag_02", 2.0, 1.0, 5000, 5000, 1, true, true, true)
-				--if IsControlJustReleased(1, 51) then
-					
-				--end
-			--end
-		--end
+
 	end)
 end
-
---[[function blowtorchAnimation()
-	ESX.ShowNotification(' llego')
-	local playerPed = GetPlayerPed(-1)
-	Citizen.CreateThread(function()
-	--while true do	
-		--Wait(100)
-		
-		while true do	
-		Wait(100)
-		end
-	--end
-	end)
-	--anim@heists@fleeca_bank@blowtorching
-	--blowtorch_right_door
-	--amb@lo_res_idles@
-	--world_human_const_blowtorch_lo_res_base
-end
-
---[[
-TASK_PLAY_ANIM(Ped ped, char* animDictionary, char* animationName, float speed, float speedMultiplier, int duration, int flag, float playbackRate, BOOL lockX, BOOL lockY, BOOL lockZ);
-
-
-]]--
