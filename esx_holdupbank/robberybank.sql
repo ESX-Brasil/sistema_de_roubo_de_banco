77b598bba4a164cca7d7600d19c0f92f09b0f030
renildomarcio <krsolucoesweb@gmail.com>
2019-06-02 08:18:28 -0300

    Update robberybank.sql

--- esx_holdupbank/robberybank.sql
+++ esx_holdupbank/robberybank.sql
@@ -1,4 +1,18 @@
 INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
-('blowtorch', 'Maçarico', 1, 0, 1),
-('c4_bank', 'C4 de 10 kilos', 1, 0, 1),
-('rasperry', 'Framboesa', 1, 0, 1);
+  ('blowtorch', 'Maçarico', 1, 0, 1),
+  ('c4_bank', 'C4 de 10 kilos', 1, 0, 1),
+  ('rasperry', 'Celular Hacker', 1, 0, 1)
+;
+
+
+INSERT INTO `shops` (name, item, price) VALUES
+	('TwentyFourSeven','blowtorch',1000),
+	('RobsLiquor','blowtorch',1000),
+	('LTDgasoline','blowtorch',1000),
+  ('TwentyFourSeven','c4_bank',1000),
+	('RobsLiquor','c4_bank',1000),
+	('LTDgasoline','c4_bank',1000),
+  ('TwentyFourSeven','rasperry',1000),
+	('RobsLiquor','rasperry',1000),
+	('LTDgasoline','rasperry',1000)
+;
